aa0ab9fae2864f485866db8d9d5eed67afff934a
t3m14 <artemon.m174@gmail.com>
2023-03-26 10:22:59 +0500

    initial

---
+++ kypcoBa9l/tests/test_views.py
@@ -0,0 +1,23 @@
+from kypcoBa9l import models
+from kypcoBa9l.views.default import my_view
+from kypcoBa9l.views.notfound import notfound_view
+
+
+def test_my_view_failure(app_request):
+    info = my_view(app_request)
+    assert info.status_int == 500
+
+def test_my_view_success(app_request, dbsession):
+    model = models.MyModel(name='one', value=55)
+    dbsession.add(model)
+    dbsession.flush()
+
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['one'].name == 'one'
+    assert info['project'] == 'lib_admin'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
